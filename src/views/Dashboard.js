10d6af9ef2b48cd37247437a3722fb517ca557b5
Issar Manknojiya <imanknoj@uwo.ca>
2025-03-25 18:19:00 -0400

    layout view updated and fixed

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "../components/Dashboard.css";
 import { Box, Button } from '@mui/material';
-import { white } from '@mui/material/colors';
-
 
 
 const Dashboard = () => {
   return (
-    <div className="label">
+    <div className="label" style={{ textAlign: "center" }}>
       <div className="text-wrapper">EmpowerHERTo</div>
-      <Box sx={{ pt: 15, ml: 60 }}>
+      <Box 
+          sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 5,
+          pt: 15,
+          flexWrap: "wrap"
+        }}>
         <Button className="custom-btn" variant="outlined" size="large" sx={{ mr: 20 }}>Monthly Earnings</Button>
         <Button className="custom-btn" variant="outlined" size="large" sx={{ mr: 20 }}>Monthly Expenses</Button>
         <Button className="custom-btn" variant="outlined" size="large">Points and Progress</Button>
