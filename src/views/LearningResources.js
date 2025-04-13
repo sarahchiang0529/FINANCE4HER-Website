b5d873db8a4b18480ed86dc79f7d80f41005c478
Sarah Chiang <schiang0529@gmail.com>
2025-04-13 19:47:19 -0400

    Update Learning Resources page

--- src/views/LearningResources.js
+++ src/views/LearningResources.js
@@ -3,21 +3,39 @@ import "../stylesheets/LearningResources.css";
 import PageWrapper from "../components/PageWrapper";
 
 export const LearningResources = () => {
+  const handleClick = (title) => {
+    console.log(`${title} clicked`);
+  };
+
   return (
-    <PageWrapper> 
+    <PageWrapper>
       <div className="desktop-5-learning-resources">
-        <div className="squares-container">
-          <div className="rectangle">
-            <div className="rectangle-title">Suggested Services</div>
-          </div>
-          <div className="rectangle">
-            <div className="rectangle-title">Investing Tips</div>
-          </div>
-          <div className="rectangle">
-            <div className="rectangle-title">Daily Tutorial</div>
-          </div>
-          <div className="rectangle">
-            <div className="rectangle-title">Saving Tips</div>
+        <div className="squares-wrapper">
+          <div className="squares-container">
+            <div
+              className="rectangle"
+              onClick={() => handleClick("Articles")}
+            >
+              <div className="rectangle-title">Articles</div>
+            </div>
+            <div
+              className="rectangle"
+              onClick={() => handleClick("Videos")}
+            >
+              <div className="rectangle-title">Videos</div>
+            </div>
+            <div
+              className="rectangle"
+              onClick={() => handleClick("Case Studies")}
+            >
+              <div className="rectangle-title">Case Studies</div>
+            </div>
+            <div
+              className="rectangle"
+              onClick={() => handleClick("Quizzes")}
+            >
+              <div className="rectangle-title">Quizzes</div>
+            </div>
           </div>
         </div>
       </div>
