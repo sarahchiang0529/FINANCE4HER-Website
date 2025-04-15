7205eb9bda1c843e30bef59fd4870d72a69cddd3
Sarah Chiang <schiang0529@gmail.com>
2025-04-15 04:04:13 -0400

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,6 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__top">
-          <nav className="footer__nav">
-            <RouterNavLink to="/" className="footer__link">Home</RouterNavLink>
-            <RouterNavLink to="/dashboard" className="footer__link">Dashboard</RouterNavLink>
-            <RouterNavLink to="/income" className="footer__link">Income</RouterNavLink>
-            <RouterNavLink to="/expenses" className="footer__link">Expenses</RouterNavLink>
-            <RouterNavLink to="/saving-goals" className="footer__link">Saving Goals</RouterNavLink>
-            <RouterNavLink to="/learning-resources" className="footer__link">Learning Resources</RouterNavLink>
-          </nav>
-        </div>
-
         <div className="footer__bottom">
           <span className="footer__copy">
             &copy; 2024 EmpowHERto.org, All Rights Reserved.
